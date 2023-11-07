a32547ad1f16659f2fd11ffa32758fdc5c4dbf06
Dibyajyoti <92978569+dibyajyoti79@users.noreply.github.com>
2023-11-08 01:04:49 +0530

    Update application.py

--- application.py
+++ application.py
@@ -13,35 +13,68 @@ def homePage():
     return render_template("index.html")
 
 
-@application.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
-@cross_origin()
-def index():
-    if request.method == 'POST':
-        try:
-            #  reading the inputs given by the user
-            Age = float(request.form['Age'])
-            Height = float(request.form['Height'])
-            Weight = float(request.form['Weight'])
-            Sit_Reach = float(request.form['Sit_Reach'])
-            fifty_m_Dash = float(request.form['fifty_m_Dash'])
-            PushUps = float(request.form['PushUps'])
-            one_min_SitUps = float(request.form['one_min_SitUps'])
-            Run_Walk = float(request.form['Run_Walk'])
-            filename = 'utsav_rf.pickle'
-            loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
-            # predictions using the loaded model file
-            prediction = loaded_model.predict(
-                [[Age, Height, Weight, Sit_Reach, fifty_m_Dash, PushUps, one_min_SitUps, Run_Walk]])
-            print('prediction is', prediction)
-            # showing the prediction results in a UI
-            return render_template('results.html', prediction=prediction)
-        except Exception as e:
-            print('The Exception message is: ', e)
-            return 'something is wrong'
-    # return render_template('results.html')
-    else:
-        return render_template('index.html')
+# @application.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+# @cross_origin()
+# def index():
+#     if request.method == 'POST':
+#         try:
+#             #  reading the inputs given by the user
+#             Age = float(request.form['Age'])
+#             Height = float(request.form['Height'])
+#             Weight = float(request.form['Weight'])
+#             Sit_Reach = float(request.form['Sit_Reach'])
+#             fifty_m_Dash = float(request.form['fifty_m_Dash'])
+#             PushUps = float(request.form['PushUps'])
+#             one_min_SitUps = float(request.form['one_min_SitUps'])
+#             Run_Walk = float(request.form['Run_Walk'])
+#             filename = 'utsav_rf.pickle'
+#             loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
+#             # predictions using the loaded model file
+#             prediction = loaded_model.predict(
+#                 [[Age, Height, Weight, Sit_Reach, fifty_m_Dash, PushUps, one_min_SitUps, Run_Walk]])
+#             print('prediction is', prediction)
+#             # showing the prediction results in a UI
+#             return render_template('results.html', prediction=prediction)
+#         except Exception as e:
+#             print('The Exception message is: ', e)
+#             return 'something is wrong'
+#     # return render_template('results.html')
+#     else:
+#         return render_template('index.html')
 
+
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    try:
+        # Parse JSON data from the request body
+        data = request.get_json()
+
+        # Extract data from the JSON
+        Age = float(data.get('Age'))
+        Height = float(data.get('Height'))
+        Weight = float(data.get('Weight'))
+        Sit_Reach = float(data.get('Sit_Reach'))
+        fifty_m_Dash = float(data.get('fifty_m_Dash'))
+        PushUps = float(data.get('PushUps'))
+        one_min_SitUps = float(data.get('one_min_SitUps'))
+        Run_Walk = float(data.get('Run_Walk'))
+
+        filename = 'utsav_rf.pickle'
+        loaded_model = pickle.load(open(filename, 'rb'))
+
+        # Make predictions using the loaded model
+        prediction = loaded_model.predict(
+            [[Age, Height, Weight, Sit_Reach, fifty_m_Dash, PushUps, one_min_SitUps, Run_Walk]])
+
+        result = {
+            "prediction": prediction.tolist()
+        }
+
+        return jsonify(result)
+    except Exception as e:
+        print('The Exception message is: ', e)
+        return jsonify({"error": "Something went wrong"})
 
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=8001, debug=True)
